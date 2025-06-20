f22afcd53e8b35da83a39eda414d42feba28a3f6
Iryna Devianina <devianinairen@gmail.com>
2025-06-20 20:26:30 +0300

    add task-2

--- js/task-2.js
+++ js/task-2.js
@@ -14,6 +14,22 @@ class Storage {
     }
 
     removeItem(itemToRemove) {
-        
+        const index = this.#items.indexOf(itemToRemove);
+        if (index !== -1) {
+            this.#items.splice(index, 1);
+        }
     }
 }
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
